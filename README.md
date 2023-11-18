7053acb00467937f7bc3d0976e0a8ab21628c534
abnerfs <abnerfs@gmail.com>
2023-11-18 16:26:32 -0300

    First version

---
+++ README.md
@@ -0,0 +1,13 @@
+# CS2 ShowDamage
+Simple showdamage plugin for CS2
+
+![image](https://github.com/abnerfs/round_end_sounds/assets/14078661/f00407ef-b7fd-4870-9de6-775e8f218deb)
+
+# Requirements
+
+[Counter Strike Sharp](https://github.com/roflmuffin/CounterStrikeSharp)
+
+# Instalation
+- Download the latest release from https://github.com/abnerfs/cs2-showdamage/releases
+- Extract the .zip file into `addons/counterstrikesharp/plugins`
+- Enjoy
---
+++ ShowDamage.cs
@@ -0,0 +1,77 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Entities;
+using System.Text;
+
+namespace ShowDamage
+{
+    class DamageDone
+    {
+        public float Health { get; set; }
+        public float Armor { get; set; }
+    }
+
+    public class ShowDamage : BasePlugin
+    {
+        public override string ModuleName => "AbNeR ShowDamage";
+
+        public override string ModuleVersion => "1.0.0";
+
+        public override string ModuleAuthor => "AbNeR_CSS";
+        
+        public override string ModuleDescription => "Shows damage dealt to enemies in the center text";
+
+        Dictionary<int, DamageDone> damageDone = new();
+
+        public override void Load(bool hotReload)
+        {
+            Console.WriteLine("Showdamage loaded");
+        }
+
+        [GameEventHandler]
+        public HookResult EventPlayerHurt(EventPlayerHurt ev, GameEventInfo info)
+        {
+            if (ev.Attacker is null ||
+                ev.Userid is null ||
+                ev.Attacker.TeamNum == ev.Userid.TeamNum)
+                return HookResult.Continue;
+
+            int attackerUserId = ev.Attacker.UserId!.Value;
+            if (damageDone.ContainsKey(attackerUserId))
+            {
+                var dmg = damageDone[attackerUserId];
+                dmg.Health += ev.DmgHealth;
+                dmg.Armor += ev.DmgArmor;
+            }
+            else
+            {
+                damageDone.Add(attackerUserId, new DamageDone { Armor = ev.DmgArmor, Health = ev.DmgHealth });
+                Action callback = () =>
+                {
+                    if (damageDone.ContainsKey(attackerUserId))
+                    {
+                        var player = Utilities.GetPlayerFromUserid(attackerUserId);
+                        if (player is not null && player.IsValid)
+                        {
+                            var dmg = damageDone[attackerUserId];
+                            if(dmg is not null)
+                            {
+                                StringBuilder builder = new();
+                                builder.Append($"-{dmg.Health} HP");
+                                if (dmg.Armor > 0)
+                                    builder.Append($"\n-{dmg.Armor} Armor");
+
+                                player.PrintToCenter(builder.ToString());
+                            }
+                        }
+                        damageDone.Remove(attackerUserId);
+                    }
+                };
+
+                NativeAPI.CreateTimer(0.1F, callback, 0);
+            }
+            return HookResult.Continue;
+        }
+    }
+}
